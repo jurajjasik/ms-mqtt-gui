3df3d50498b3a035970fae644927564805cee8c9
Jasik <juraj.jasik@gmail.com>
2024-08-27 15:28:43 +0200

    Update ms_logic.py

--- ms_logic.py
+++ ms_logic.py
@@ -1,30 +1,103 @@
 import json
 import logging
-from datetime import datetime
 import threading
+from datetime import datetime
+
+import paho.mqtt.client as mqtt
+from paho.mqtt.client import CallbackAPIVersion
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
-TIMEOUT_CONFIRMATION = 0.05  # seconds
+TIMEOUT_CONFIRMATION = 60  # seconds
+
 
 def unique_confirmation_id():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
 
+
 class MSLogic:
-    def __init__(self):
+    def __init__(self, config):
+        self.should_stop = None
         self.confirmation_events = {}
-    
-    def confirme(self, confirmation_id):
-        event = self.confirmation_events.get(confirmation_id)
-        if event is not None:
-            event.set()
-        
+        self.topic_base_mass_filter = config["topic_base_mass_filter"]
+        self.topic_base_electromer = config["topic_base_electromer"]
+        self.device_name_mass_filter = config["device_name_mass_filter"]
+        self.device_electromer = config["device_electromer"]
+
+        self.client = mqtt.Client(
+            callback_api_version=CallbackAPIVersion.VERSION2,
+            clean_session=True,
+        )
+
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+    def set_stop_test(self, should_stop):
+        self.should_stop = should_stop
+
+    def start(self):
+        self.client.connect(
+            self.config["mqtt_broker"],
+            self.config["mqtt_port"],
+            self.config["mqtt_connection_timeout"],
+        )
+        self.client.loop_start()
+
+    def on_connect(self, client, userdata, flags, rc, properties):
+        log.debug(f"Connected with result code {rc}")
+
+        self.client.subscribe(
+            f"{self.topic_base_mass_filter}/response/{self.topic_base_electromer}/#"
+        )
+        self.client.subscribe(
+            f"{self.topic_base_mass_filter}/error/{self.topic_base_electromer}"
+        )
+
+        self.client.subscribe(
+            f"{self.topic_base_electromer}/response/{self.device_electromer}/#"
+        )
+        self.client.subscribe(
+            f"{self.topic_base_electromer}/error/{self.device_electromer}"
+        )
+
+    def on_message(self, client, userdata, message):
+        topic = message.topic
+        try:
+            payload = json.loads(message.payload.decode())
+        except json.JSONDecodeError as e:
+            log.debug(f"Error decoding message payload: {e}")
+            payload = {}
+
+        log.debug(f"Received message on topic {topic} with payload {payload}")
+
+        if topic.startswith(
+            f"{self.topic_base_mass_filter}/error/{self.topic_base_electromer}/"
+        ):
+            raise Exception(f"Error from mass filter: {payload}")
+        elif topic.startswith(
+            f"{self.topic_base_electromer}/error/{self.device_electromer}/"
+        ):
+            raise Exception(f"Error from electromer: {payload}")
+        elif topic.startswith(
+            f"{self.topic_base_mass_filter}/response/{self.topic_base_electromer}/"
+        ):
+            if topic.endswith("mz"):
+                self.handle_response_mz(payload)
+        elif topic.startswith(
+            f"{self.topic_base_electromer}/response/{self.device_electromer}/"
+        ):
+            if topic.endswith("current"):
+                self.handle_response_current(payload)
+
     def confirme_payload(self, payload):
         if "confirmation_id" in payload:
             confirmation_id = payload["confirmation_id"]
-            self.confirme(confirmation_id)
-            
+            event = self.confirmation_events.get(confirmation_id)
+            if event is not None:
+                event.set()
+                event.payload = payload
+
     def wait_for_confirmation(self, confirmation_id):
         """
         Waits for the confirmation with the specified ID.
@@ -40,11 +113,20 @@ class MSLogic:
         """
         event = threading.Event()
         self.confirmation_events[confirmation_id] = event
-        try:
-            event.wait(timeout=TIMEOUT_CONFIRMATION)
-        finally:
+
+        for t in range(TIMEOUT_CONFIRMATION):
+            flag = event.wait(timeout=1)
+            if flag or (self.should_stop is not None and self.should_stop()):
+                break
+
+        if not flag:
             del self.confirmation_events[confirmation_id]
-    
+            raise TimeoutError(f"Timeout waiting for confirmation {confirmation_id}")
+
+        payload = event.payload
+        del self.confirmation_events[confirmation_id]
+        return payload
+
     def publish_set_mz(self, mz):
         # publish MQTT message to set the m/z
         confirmation_id = unique_confirmation_id()
@@ -53,9 +135,22 @@ class MSLogic:
             json.dumps({"value": mz, "confirmation_id": confirmation_id}),
         )
         return confirmation_id
-    
+
+    def publish_measure_current(self):
+        # publish MQTT message to measure the current
+        confirmation_id = unique_confirmation_id()
+        self.client.publish(
+            f"{self.topic_base_electromer}/cmnd/{self.device_electromer}/current",
+            json.dumps({"confirmation_id": confirmation_id}),
+        )
+        return confirmation_id
+
     def handle_response_mz(self, payload):
         # handle the response from the mass filter
+        self.confirme_payload(payload)
+
+    def handle_response_current(self, payload):
+        # handle the response from the electromer
         self.confirme_payload(payload)
 
     def configure_electromer(self):
@@ -68,27 +163,41 @@ class MSLogic:
 
     def set_mz(self, mz):
         """
-        Sets the mz value and waits for confirmation.
+        Sets the m/z value and waits for confirmation.
 
         Args:
-            mz: The mz value to be set.
+            mz: The m/z value to be set.
 
         Returns:
-            None
-            
+            The value of the mz that was set.
+
         Raises:
             TimeoutError: If the confirmation is not received within the timeout.
         """
         confirmation_id = self.publish_set_mz(mz)
-        self.wait_for_confirmation(confirmation_id)
+        payload = self.wait_for_confirmation(confirmation_id)
+        if "value" in payload:
+            return payload["value"]
+        else:
+            return None
 
     def measure_current(self):
-        # log.debug("Measuring current")
+        """
+        Measures the current and returns the value.
 
-        # TODO: Implement the actual measurement
-        import random
+        Returns:
+            float: The measured current value.
+            None: If the current value cannot be measured.
 
-        return random.random()
+        Raises:
+            TimeoutError: If the confirmation is not received within the timeout.
+        """
+        confirmation_id = self.publish_measure_current()
+        payload = self.wait_for_confirmation(confirmation_id)
+        if "value" in payload:
+            return payload["value"]
+        else:
+            return None
 
     def get_metadata_mass_filter(self):
         # TODO: Implement the actual metadata\
